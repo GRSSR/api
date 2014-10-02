f67f13d9a4b5fb3eae2110ee54d574508d76d4a0
Taran Moltu <taranmoltu@gmail.com>
2014-10-03 00:58:13 +0100

    rewrote the splot function and added a hello feature to the soviet protocol

--- redString.lua
+++ redString.lua
@@ -1,19 +1,37 @@
-
 function split(string, limit, delimiter)
 	if not delimiter then
-		delimiter = "%S+"
+		delimiter = "%s+"
 	end
 	local ret = {}
 	local length = 0
-	for match in string.gmatch(string, delimiter) do
-		if limit and (#ret + 1) >= limit then
-			local start, finish = string:find(match, length, true)
-			ret[#ret + 1] = string:sub(start)
-			break
+
+	while true do
+		local dStart, dEnd = string:find(delimiter)
+		if limit and length >= limit -1 then
+			ret[#ret + 1] = string
+			return ret
+		elseif dStart then
+			if dEnd >= string:len()+1 then
+				return ret
+			end
+
+			ret[#ret + 1] = string:sub(1, dStart-1)
+			length = length + 1
+			string = string:sub(dEnd+1)
+		elseif string:len() == 0 then
+			return ret
 		else
-			ret[#ret + 1] = match
-			length = length + ret[#ret]:len()
+			ret[#ret + 1] = string
+			return ret
 		end
 	end
-	return ret
+end
+
+function join(list, delimiter)
+	if not delimiter then delimiter = " " end
+	local string = ""
+	for k, value in pairs(list) do 
+		string = string .. delimiter .. value
+	end
+	return string
 end
--- sovietProtocol.lua
+++ sovietProtocol.lua
@@ -2,26 +2,33 @@ os.loadAPI("/api/redString")
 
 local modem = nil
 local protocolChannel = 1
-
+local LISTEN_CHANNEL = 1
+local PROTOCOL_CHANNEL = 1
 local debug = 0
 
 function setDebugLevel(value)
 	debug = value
 end
 
-function init(protoChannel, listenChannel, side)
+function init(protoChannel, listenChannel, side, protocol)
 	if side == nil then
 		modem = peripheral.find("modem")
 	else
 		modem = peripheral.wrap(side)
 	end
-	protocolChannel = protoChannel
+
+	if not protocol then protocol = "generic" end
+
+	PROTOCOL_NAME = protocol
+	PROTOCOL_CHANNEL = protoChannel
+	LISTEN_CHANNEL = listenChannel
 	modem.open(listenChannel)
 end
 
 function parse(message)
 	local split = redString.split(message, 3)
 	ret = {}
+	ret.protocol = ""
 	ret.method = split[1]
 	ret.id = split[2]
 	ret.body = split[3]
@@ -29,7 +36,6 @@ function parse(message)
 end
 
 function send(sendChannel, replyChannel, method, id, body)
-	if not body then body = "" end
 	local message = method
 	if id then 
 		message = message.." "..id
@@ -43,13 +49,47 @@ function send(sendChannel, replyChannel, method, id, body)
 	modem.transmit(sendChannel, replyChannel, message)
 end
 
-function listenRaw()
-	local event, modemSide, senderChannel, replyChannel,
-		message, senderDistance = os.pullEvent("modem_message")
-	if debug > 5 then
-		print("Recieved: "..message:sub(1, 40))
+function hello(sendChannel, protocol)
+	send(sendChannel, LISTEN_CHANNEL, "hello", protocol)
+	local function helloCheck()
+		local reply, response = listen()
+		if response.method == "greeting" then
+			return true, redString.split(response.body)
+		else
+			return false, {}
+		end
 	end
-	return replyChannel, message
+
+	local function sleep1()
+		sleep(1)
+	end
+
+	local winner = parallel.waitForAny(helloCheck, sleep1)
+
+	if winner == 1 then
+		return true
+	else
+		return false
+	end
+	
+end
+
+function listenRaw()
+	while true do
+		local event, modemSide, senderChannel, replyChannel,
+			message, senderDistance = os.pullEvent("modem_message")
+		if parse(message).method == "hello" then
+			if debug > 7 then
+				print("Recieved hello from "..replyChannel)
+			end
+			send(replyChannel, LISTEN_CHANNEL, "greeting", os.getComputerID(), PROTOCOL_NAME)
+		else
+			if debug > 5 then
+				print("Recieved: '"..message:sub(1, 40).."'")
+			end
+			return replyChannel, message
+		end
+	end
 end
 
 function listen()
